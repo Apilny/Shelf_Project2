e6cf10c28c211a1fa377fbb53ed1cc007dfe9def
jeremydabbs <56282992+jeremydabbs@users.noreply.github.com>
2019-11-19 14:31:20 -0600

    Update urls.py
    Added aisle search

--- apps/shelf_app/urls.py
+++ apps/shelf_app/urls.py
@@ -14,6 +14,7 @@ urlpatterns=[
     url(r'shelf/(?P<location_id>\d+)/(?P<food_id>)/edit$', views.edit_item_at_location),
     url(r'shelf/(?P<location_id>\d+)/(?P<food_id>)/update$', views.update_item_at_location),
     url(r'shelf/(?P<aisle_id>\d+)/aisle$',views.view_aisle_items),
+    url(r'shelf/(?P<aisle_id>\d+)/aisle/search$',views.aisle_search),
     url(r'shelf/create/store$',views.create_store),
     url(r'shelf/maps', views.show_map),
     url(r'^logout$', views.logout)
